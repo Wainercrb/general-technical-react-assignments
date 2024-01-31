5fad4d1659b8a52ae7ce5ea21fed6fdeb301ece7
wainerrodriguez <weiner.rodriguez@costaricasoftwareservices.com>
2024-01-31 14:51:40 -0600

    Added basic logic for OPT configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import OtpPage from './otp/Index';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/otp-autocomplete" element={<OtpPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
